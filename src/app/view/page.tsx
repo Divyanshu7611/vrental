52e197f3eabd4d10fe2dfe8c5ba75f92c56960ad
Divyanshu Sharma <dshapbss202@gmail.com>
2024-08-22 21:40:15 +0530

    security added

--- src/app/view/page.tsx
+++ src/app/view/page.tsx
@@ -82,17 +82,27 @@
 // }
 "use client";
 
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import useApartmentData from "@/hooks/userApartmentData";
 import Navbar from "@/components/global/Navbar";
 import Footer from "@/components/global/Footer";
 import ApartmentDetails from "@/components/view/ApartmentDetails";
 import OwnerDetails from "@/components/view/OwnerDetails";
+import { useEffect } from "react";
 
 export default function Page() {
   const searchParams = useSearchParams();
   const id = searchParams.get("apartmentID");
   const { data, loading } = useApartmentData(id);
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      alert("Please Login Before Visiting To Apartment");
+      router.push("/auth");
+    }
+  }, []);
 
   return (
     <div className="min-w-screen min-h-screen bg-gradient-to-b from-[#F8F8F8] to-[#00E0FF]">
